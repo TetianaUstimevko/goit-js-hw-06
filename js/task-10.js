2a07c0c23a6036dc623c9284f57bf6d275270125
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-02-26 20:59:07 +0200

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,12 +1,35 @@
 const controls = document.querySelector('#controls');
 const createButton = controls.querySelector('[data-create]');
 const destroyButton = controls.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#controls');
+const boxesContainer = document.querySelector('#boxes');
 
+createButton.addEventListener('click', () => {
+  const amount = Number(controls.querySelector('input').value);
+  createBoxes(amount);
+});
 
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 30;
+  boxesContainer.innerHTML = '';
+  for (let i = 0; i < amount, i++) {
+    const box = document.createElement('div');
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
