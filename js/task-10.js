db058aa4530c34278ce6f4b7918a259489cba9fb
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-02-26 21:05:26 +0200

    create task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,35 +1,35 @@
-const controls = document.querySelector('#controls');
-const createButton = controls.querySelector('[data-create]');
-const destroyButton = controls.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#boxes');
-
-createButton.addEventListener('click', () => {
-  const amount = Number(controls.querySelector('input').value);
-  createBoxes(amount);
-});
-
-destroyButton.addEventListener('click', destroyBoxes);
-
-function createBoxes(amount) {
-  let size = 30;
-  boxesContainer.innerHTML = '';
-  for (let i = 0; i < amount, i++) {
-    const box = document.createElement('div');
-    box.style.width = size + 'px';
-    box.style.height = size + 'px';
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-    size += 10;
-  }
-
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
-};
+    .padStart(6, "0")}`;
+}
+
+function createBoxes(amount) {
+  const boxes = document.querySelector("#boxes");
+  let dimensions = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${dimensions}px`;
+    box.style.height = `${dimensions}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(box);
+    dimensions += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxes = document.querySelector("#boxes");
+  boxes.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+createButton.addEventListener("click", () => {
+  const amount = parseInt(document.querySelector("input").value);
+  createBoxes(amount);
+});
+
+const destroyButton = document.querySelector("[data-destroy]");
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
