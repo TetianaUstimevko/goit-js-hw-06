26d1cb7b6bdf1ee4a88c0420f1bff8407b406090
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-02-26 20:37:35 +0200

    add part task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,10 @@
+const controls = document.querySelector('#controls');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#controls');
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
