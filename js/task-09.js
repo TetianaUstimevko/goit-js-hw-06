d146caa1c4a8666322cc6cb56cfd5a0490d877a6
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-02-26 20:09:08 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,20 @@
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+function handleButtonClick() {
+
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+
+  colorSpan.textContent = randomColor;
+};
+
+button.addEventListener('click', handleButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
